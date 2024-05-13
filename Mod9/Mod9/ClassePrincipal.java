8c89846709bdd9f1a3c5078289bef5658bdc5ade
Marcos Alemida <marcosoficial1920@gmail.com>
2024-05-13 19:38:01 -0300

    Exercicio do Mod9

---
+++ Mod9/Mod9/ClassePrincipal.java
@@ -0,0 +1,16 @@
+public class ClassePrincipal {
+    
+   
+    
+    public static void main(String[] args) {
+        
+       int i = 1111111111;
+       
+       Integer b = new Integer(i);
+
+       System.out.println(b);
+        
+    }
+    
+
+}
